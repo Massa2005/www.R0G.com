e9cb96493fad83fd02e3d764fb8aad27d78d267c
Massa Marco <marco.massa@cz.lan>
2024-05-10 12:05:53 +0200

    index fixato

--- pages/dev-pre-login.php
+++ pages/dev-pre-login.php
@@ -1,4 +1,10 @@
 <link rel="stylesheet" href="../mainStyle.css">
+<style>
+    body{
+        background-color:black;
+        overflow-y:hidden;   
+    }
+</style>
 <html>
     <?php
         ini_set('display_errors','Off');
@@ -8,24 +14,28 @@
 
     ?>
     <a href="../index.php">
-        <img src="../sources/Logo.png" id="logo">
+        <img src="../sources/Logo.png" id="logo" style="height:250px; width: 500px; left: -30px;top:50px;">
     </a>
-    <div class="borderContainer center" style="top:250px">
+    <div class="borderContainer center" style="width: fit-content; top:100px;">
+    <div class="center rightFontLogginANDRegister" style="color:rgba(239,49,128,255);font-size:50px;top:-50px;text-shadow: 0px 0px 20px rgba(239,49,128,255)">
+        Benvenuto Developer
+    </div>
         <form action="../phps/dev-login.php" method="POST">
-            <div class="center rightFont" style="width: fit-content;font-size:30px;">Mail</div>
+            <div class="center rightFontLogginANDRegister" style="width: fit-content;font-size:30px;">Mail</div>
             <input type="text" id="mail" name="mail" class="center"><br><br>
             <div class="center" id="error2" style="width: fit-content; color:red;"></div>
 
-            <div class="center rightFont"  style="width: fit-content;font-size:30px;">Password</div>
+            <div class="center rightFontLogginANDRegister"  style="width: fit-content;font-size:30px;">Password</div>
             <input type="password"  name="password" class="center inputForField"><br><br>
             <div class="center" id="error1" style="width: fit-content; color:red;"></div>
             <input type="submit" value="Login" class="center Loginbutton">
         </form>
-    </div><br>
-    
-    <div class="center rightFont" style="top:250px">
-        <a href="dev-pre-register.php" >you haven't registered yet?</a>
+        
+        <a href="dev-pre-register.php" class="center" style="left: 110px;top: 40px;font-size:25px;">you haven't registered yet?</a>
     </div>
+    
+    
+    
 </html>
 <script>
     let inmail = document.getElementById("in-mail");
